cc025d87aac6ffa73f30f5ae27b3ab93a0a2de8d
kanikapoor-dot <kanikapoor24@gmail.com>
2026-02-09 12:07:39 +0530

    Tested With picture path for photoscanning functionality.Successfull

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,22 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "photoscanner.h"
+#include <QStandardPaths>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QString picturesPath = QStandardPaths::writableLocation(QStandardPaths::PicturesLocation);
+    QStringList images = PhotoScanner::scanDirectory(picturesPath);
+    qDebug() << "Scanning path: " << picturesPath;
+    qDebug() << "found" << images.size() << "Images.";
+    if(!images.isEmpty())
+    {
+        qDebug() << "First image found" << images.first();
+    }
 }
 
 MainWindow::~MainWindow()
